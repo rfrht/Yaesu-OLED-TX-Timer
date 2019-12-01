fbaeb74562eb320a04bb138e22f79801baa3a40b
Rodrigo Freire <rfreire@rf.usersys.redhat.com>
2019-12-01 20:09:59 -0300

    Timer new feature: alert threshold
    * Featuring a new alert timer - invert the display when transmission is
      over a predetermined threshold
    * Made splash screen selectable
    * Make more extensive use of variables
    * A few more comments

--- yaesu-oled-tx-timer.ino
+++ yaesu-oled-tx-timer.ino
@@ -1,3 +1,4 @@
+// Libraries to be used
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -9,15 +10,28 @@
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-uint32_t t; // TX time (secs)
-uint8_t m;  // Derived Minutes
-uint8_t s;  // Derived seconds (60-second fraction)
+// Configure the GPIO pins
+#define TX_GND 2          // The GPIO pin where you connected the TXGND signal
+#define PULLUP 13         // The GPIO pin that is driving TXGND up
 
+// Configure a time threshold (seconds)
+// When reaching this threshold, the display will invert so it can catch
+// your attention. 
+#define TIME_ALERT 360  // 6 minutes alert
+
+// Your callsign. Comment out to disable splash screen.
+#define CALLSIGN "PY2RAF"
+
+// General variables
+uint32_t t;         // TX time (secs)
+uint8_t m;          // Derived Minutes
+uint8_t s;          // Derived seconds (60-second fraction)
 int LineState;      // Current GPIO line state
 int LastLineState;  // Previous poll GPIO line state
 
 void setup() {
   Serial.begin(9600);  // Setup serial port in case of display failure
+  
   // Display initialization
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
@@ -26,44 +40,49 @@ void setup() {
   }
 
   // Splash Screen
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(3);
-  display.setCursor(10,6);
-  display.print("PY2RAF");  // YOUR CALLSIGN HERE!! \o/
-  display.display();
-  delay(4000);
-  display.invertDisplay(true);
-  display.display();
-  delay(300);
-  display.invertDisplay(false);
-  display.display();
-  delay(300);
-  display.clearDisplay();
-
+  #ifdef CALLSIGN
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(3);
+    display.setCursor(10,6); // a small padding
+    display.print(CALLSIGN);
+    display.display();
+    delay(4000);
+    display.invertDisplay(true);
+    display.display();
+    delay(300);
+    display.invertDisplay(false);
+    display.display();
+    delay(300);
+  #endif
+  
   // Configure display runtime defaults
+  display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(4);
 
-  // Use pin 13 as a output to generate the 5V to drive pin 2 to the default high state
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  // Configure the pin that will be providing the pullup level to TX_GND
+  pinMode(PULLUP, OUTPUT);
+  digitalWrite(PULLUP, HIGH);
 
-  // Pin 2 will be used as input, where we will monitor the line level
-  pinMode(2, INPUT);
+  // Configure the TX_GND pin as a input GPIO
+  pinMode(TX_GND, INPUT);
 }
 
 void loop() {
-  // Get current state from line 2
-  LineState = digitalRead(2);
+  // Get current state from the TX_GND pin/GPIO
+  LineState = digitalRead(TX_GND);
+
   // If voltage is present - line 13 driving and no draw from TX_GND line
   if (LineState == HIGH) {
     // Do nothing - display off; flash a dot at the bottom of the screen
     display.clearDisplay();
+    display.invertDisplay(false);
     if ( millis() / 1000 % 2 == 0 ) display.drawPixel(127, 31, WHITE);
     display.display();
     delay(500);
     }
+
   // However if line is grounded... (meaning: TX is ON)
   else {
     // Was line state changed from the last poll? If it was changed (new transition),
@@ -78,7 +97,7 @@ void loop() {
 
     // Clear the display buffer and prepare for new data
     display.clearDisplay();
-    display.setCursor(0,0);
+    display.setCursor(5,2); // Better screen adjustment
 
     // Populate the timer. Used the 'if' trick to pad the seconds and minutes with a zero
     // when the actual number count is less than 10
@@ -86,13 +105,20 @@ void loop() {
     display.print(m); display.print(":");
     if (s < 10) display.print(0);
     display.print(s);
+
+    // Are we over time alert threshold?
+    #ifdef TIME_ALERT 
+      if (t > TIME_ALERT) display.invertDisplay(true);
+    #endif
     display.display();
 
     // Give it a second to sleep until next poll
     delay(1000);
+
     // Add another second to the timer
     t++;
     }
- // Save state line state from this poll run.
- LastLineState =  LineState;
+
+  // Save state line state from this poll run.
+  LastLineState =  LineState;
 }
