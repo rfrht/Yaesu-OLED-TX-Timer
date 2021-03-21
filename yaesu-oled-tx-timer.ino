307b3bdf234261cabcf56a125eb2e21fd6b1fd00
Rodrigo Freire <rfrht@users.noreply.github.com>
2021-03-21 15:25:07 -0300

    New functionalities
    - Fan Control
    - Better code indenting
    - Uptime now calculate in days/hours after 24h of uptime

--- yaesu-oled-tx-timer.ino
+++ yaesu-oled-tx-timer.ino
@@ -5,55 +5,50 @@
 #include <Adafruit_SSD1306.h>
 
 // Configure the Display
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 32  // OLED display height, in pixels
-#define OLED_RESET     4  // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH  128   // OLED display width, in pixels
+#define SCREEN_HEIGHT  32   // OLED display height, in pixels
+#define OLED_RESET      4   // Reset pin # (or -1 if sharing Arduino reset pin)
+
+// Setup the display driver with the above settings
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Are we using a temperature sensor? If we are not, comment this line.
 // The Temperature Sensor code is compatible with LM-35 sensors.
-#define TEMP_SENS      A0 // The Port where the temperature is being read
+#define TEMP_SENS      A0   // The Port where the temperature is being read
 
 // Configure the GPIO pins
-#define TX_GND         2  // The GPIO pin where you connected the TXGND signal
-#define PULLUP        13  // The GPIO pin that is driving TXGND up
-#define RX_ON          4  // The GPIO pin where you connected the SQL signal
-                          // Comment out to disable monitoring RX.
-
-// Configure a time threshold (seconds)
-// When reaching this threshold, the display will invert so it can catch
-// your attention. 
-#define TIME_ALERT 360    // 6 minutes alert
-
-// Your callsign. Comment out to disable splash screen.
-//#define CALLSIGN "PY2RAF"
-
-// Do we want to show the radio temperature in TX mode?
-#define TEMP_ON_TX 1
-
-// Monitor squelched time? If not desired, comment.
-#define MONITOR_SQUELCH 1
-
-// Monitor uptime? If not desired, comment.
-#define MONITOR_UPTIME 1
-
-// Print SQ/RX state? If not desired, comment.
-#define MONITOR_STATE 1
+#define TX_GND          2   // The GPIO pin where you connected the TXGND signal
+#define PULLUP         13   // The GPIO pin that is driving TXGND up
+#define GPIO_FAN       10   // The GPIO pin where we control the fan relay.
+#define RX_ON           4   // The GPIO pin where you connected the SQL signal
+                            // Comment out to disable monitoring RX.
+#define TIME_ALERT    120  // TX time alert threshold (seconds)
+#define CALLSIGN "PY2RAF"  // Your callsign. Comment out to disable splash screen.
+#define TEMP_ON_TX      1  // Do we want to show the radio temperature in TX mode?
+#define TEMP_THRESHOLD 40  // Starts external fan when temp greater than this
+#define MONITOR_SQUELCH 1  // Monitor squelched time? If not desired, comment.
+#define MONITOR_UPTIME  1  // Monitor uptime? If not desired, comment.
+#define MONITOR_STATE   1  // Print SQ/RX state? If not desired, comment.
 
 // General variables
-uint32_t t;         // Timer (secs)
-uint8_t h;          // Derived hours
-uint8_t m;          // Derived Minutes
-uint8_t s;          // Derived seconds (60-second fraction)
-uint32_t u;         // Uptime (secs)
-uint8_t hu;         // Derived uptime hours
-uint8_t mu;         // Derived uptime Minutes
-int temperature;    // Current temp; Celsius
-
-String LastState;   // The last active state used for proper timer tracking
+uint32_t t;             // Timer (secs)
+uint8_t h;              // Derived hours
+uint8_t m;              // Derived Minutes
+uint8_t s;              // Derived seconds (60-second fraction)
+uint32_t u;             // Uptime (secs)
+uint8_t mu;             // Derived uptime minutes
+uint8_t hu;             // Derived uptime hours
+uint8_t du;             // Derived uptime days
+int temperature;        // Current temp; Celsius
+int temp_high_counter;  // Temperature above Threshold counter
+int temp_low_counter;   // Temperature under Threshold counter
+bool fan_state;         // Self Explanatory
+String LastState;       // The last active state used for proper timer tracking
 
 void setup() {
-  Serial.begin(9600);  // Setup serial port in case of display failure
+   u = 0;              // Start uptime with zero seconds
+
+  Serial.begin(9600);   // Setup serial port for debug in case of display failure
 
   // Display initialization
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -64,6 +59,12 @@ void setup() {
 
   #ifdef RX_ON
     pinMode(RX_ON, INPUT);
+  #endif
+
+  // If we are controlling a fan...
+  #ifdef GPIO_FAN
+    pinMode(GPIO_FAN, OUTPUT);
+    digitalWrite(GPIO_FAN, LOW);
   #endif
 
   // Splash Screen
@@ -96,50 +97,71 @@ void setup() {
 }
 
 #ifndef MONITOR_SQUELCH
-// Blinking dot function
+// Blinking dot every other second
 void blinkingdot() {
   display.clearDisplay();
   display.invertDisplay(false);
-  if ( millis() / 1000 % 2 == 0 ) display.drawPixel(127, 31, WHITE);
+  if ( u % 2 == 0 ) display.drawPixel(127, 31, WHITE);
   display.display();
   delay(500);
 }
 #endif
 
 #ifdef MONITOR_UPTIME
-// Print the current uptime
-void print_uptime(){
-  u = millis()/1000;
-  mu = u / 60 % 60;
-  hu = u / 3600;
+  // Print the current uptime
+  void print_uptime(){
+    mu = u / 60 % 60;
+    hu = u / 3600;
+    du = u / 86400;
+    u++;
 
-  // Uptime block
-  display.setTextSize(2);  // Medium size
-  display.setCursor(0, 0); // Bottom top of the screen
-  if (hu < 10) display.print(0);
-  display.print(hu); display.print("h");
-  if (mu < 10) display.print(0);
-  display.print(mu);
-  display.setTextSize(1);
+    // Uptime block
+    display.setTextSize(2);  // Medium size
+    display.setCursor(0, 0); // Bottom top of the screen
+    if (du < 1 ) {           // Up to a day; hh:mm
+      if (hu < 10) display.print(0);
+      display.print(hu); display.print("h");
+      if (mu < 10) display.print(0);
+      display.print(mu);
+    }
+    else {                   // More than a day; dd:hh
+      hu = hu - ( du * 86400 / 3600);   // Calculate how many hours within the day.
+      if (du < 10) display.print(0);
+      display.print(du); display.print("d");
+      if (hu < 10) display.print(0);
+      display.print(hu);
+      if (du < 100) display.print("h"); // Strips the H suffix after 100 days
+    }
+    display.setTextSize(1);
 
-  // The temperature block
-  #ifdef TEMP_SENS
-  // The uptime text
-  display.setCursor(12,16);
-  display.print("Uptime");
-  // The temperature info
-  temperature = (float(analogRead(TEMP_SENS))*5/(1023))/0.01;
-  display.setCursor(7,25);
-  display.print("Temp:");
-  display.print(temperature);
-  display.print("C");
-  #else
-  // If no temperature selected, just print uptime.
-  display.setCursor(12,24);
-  display.print("Uptime");
-  #endif
+    // The temperature block
+    #ifdef TEMP_SENS
+      // The uptime text
+      display.setCursor(12,16);
+      display.print("Uptime");
+      // The temperature info
+      display.setCursor(7,25);
+      display.print("Temp:");
+      display.print(temperature);
+      display.print("C");
 
-}
+      // Blink a dot when fan is on
+      #ifdef GPIO_FAN
+        if (fan_state == 1) {
+          if ( u % 2 == 0 ) {
+            display.setCursor(69,15);
+            display.print((char)7); 
+          }
+        }
+      #endif
+
+    #else
+      // If no temperature selected, just print uptime.
+      display.setCursor(12,24);
+      display.print("Uptime");
+    #endif
+
+  }
 #endif
 
 // Print Squelched timer
@@ -159,13 +181,13 @@ void printsquelch() {
   display.clearDisplay();
 
   #ifdef MONITOR_UPTIME
-  print_uptime();
+    print_uptime();
   #endif
 
   #ifdef MONITOR_STATE
-  display.setTextSize(2);
-  display.setCursor(92,0);
-  display.print("SQ");
+    display.setTextSize(2);
+    display.setCursor(92,0);
+    display.print("SQ");
   #endif
 
   display.setTextSize(1);  // Smaller size
@@ -190,18 +212,45 @@ void printsquelch() {
 }
 
 void loop() {
-    // If voltage is present - meaning no Transmission; radio receiving or squelched
-    if (digitalRead(TX_GND) == HIGH) {
+  #ifdef TEMP_SENS
+    temperature = (float(analogRead(TEMP_SENS))*5/(1023))/0.01;
+  #endif
 
-      // If we are monitoring RX time too
-      #ifdef RX_ON
+  // Control logic for fan. 
+  // Runs the fan if temperature gets above threshold for 15 seconds in row.
+  // Stops the fan if temperature gets below threshold for 2 minutes in row.
+  #ifdef GPIO_FAN
+    if (temperature > TEMP_THRESHOLD) {
+      temp_high_counter++;
+      temp_low_counter=0; }
+    else if (fan_state == 1 && temp_low_counter < 120 ) {
+      temp_low_counter++;
+      temp_high_counter=0; }
+    else {
+      temp_low_counter++;
+      temp_high_counter=0;
+      digitalWrite(GPIO_FAN, LOW);
+      fan_state=0;
+    }
+
+    if ( temp_high_counter > 15 ) {
+      digitalWrite(GPIO_FAN, HIGH);
+      fan_state=1; 
+    }
+  #endif
+
+  // If voltage is present - meaning no Transmission; radio receiving or squelched
+  if (digitalRead(TX_GND) == HIGH) {
+
+    // If we are monitoring RX time too
+    #ifdef RX_ON
 
       // Yaesu radio: Squelch Open, RX=HIGH
       if (digitalRead(RX_ON) == HIGH) {
       // Icom and Kenwood radio: Squelch open, RX=LOW
       // Comment the above "if" and uncomment the below "if".
       // (digitalRead(RX_ON) == LOW) {
-      
+
         // Reset timer if transitioned state
         if (LastState != "RX") {
           t=0;
@@ -216,12 +265,12 @@ void loop() {
         // Clear the display buffer and prepare for new data
         display.clearDisplay();
         #ifdef MONITOR_UPTIME
-        print_uptime();
+          print_uptime();
         #endif
         #ifdef MONITOR_STATE
-        display.setTextSize(2);
-        display.setCursor(92,15);
-        display.print("RX");
+          display.setTextSize(2);
+          display.setCursor(92,15);
+          display.print("RX");
         #endif
         display.setTextSize(1);  // Smaller size
         display.setCursor(80,0); // Better screen adjustment
@@ -244,26 +293,26 @@ void loop() {
         LastState = "RX";
       }
 
-    else {
-      // Print a blinking dot or squelch counter
-      #ifndef MONITOR_SQUELCH
-        blinkingdot();
-      #else
-        printsquelch();
-      #endif
+      else {
+        // Print a blinking dot or squelch counter
+        #ifndef MONITOR_SQUELCH
+          blinkingdot();
+        #else
+          printsquelch();
+        #endif
+      }
     }
-  }
 
-  // If we do not monitor RX stage, insert this block
-  #else
-      blinkingdot();
-      LastState = "Squelch";
-  }
-  #endif
+    // If we are NOT monitoring RX time
+    #else
+        blinkingdot();
+        LastState = "Squelch";
+      }
+    #endif
 
   // However if line is grounded... (meaning: TX is ON)
   else {
-    // Reset timer if transitioned state
+    // Reset timer if radio changed state
     if (LastState != "TX") {
       t=0 ;
       display.invertDisplay(false);
@@ -284,23 +333,40 @@ void loop() {
     display.print(m); display.print(" ");
     if (s < 10) display.print(0);
     display.print(s);
-    
+
     #ifdef TEMP_SENS
-    #ifdef TEMP_ON_TX
-    temperature = (float(analogRead(TEMP_SENS))*5/(1023))/0.01;
-    display.drawRect(54,9,18,15,WHITE);
-    display.setTextSize(1);
-    display.setCursor(57,13);
-    display.print(temperature);
-    #else
-    display.setTextSize(4);
-    display.setCursor(55,2); // Print colon
-    display.print(":");
-    #endif
-    #else
-    display.setTextSize(4);
-    display.setCursor(55,2); // Print colon
-    display.print(":");
+      #ifdef TEMP_ON_TX
+        #ifdef GPIO_FAN
+          if (fan_state == 1 ) { // Fan on, temperature font/color inverted
+            display.fillRect(54,9,18,15,WHITE);
+            display.setTextSize(1);
+            display.setTextColor(BLACK);
+            display.setCursor(58,13);
+            display.print(temperature); 
+            display.setTextColor(WHITE); }
+          else {  // Fan off, just temperature inside a box.
+            display.drawRect(54,9,18,15,WHITE);
+            display.setTextSize(1);
+            display.setCursor(58,13);
+            display.print(temperature); }
+
+        #else // No Fan Control
+          display.drawRect(54,9,18,15,WHITE);
+          display.setTextSize(1);
+          display.setCursor(58,13);
+          display.print(temperature); }
+        #endif
+
+      #else // No Temperature on TX
+        display.setTextSize(4);
+        display.setCursor(55,2); // Print colon
+        display.print(":");
+      #endif
+
+    #else // No Temperature sensor
+      display.setTextSize(4);
+      display.setCursor(55,2); // Print colon
+      display.print(":");
     #endif
 
     // Are we over time alert threshold?
